ea30c08e54823aa2e552d8afccf273e7495b5bb0
NightWholfBR <127116185+NightWholfBR@users.noreply.github.com>
2023-09-25 10:41:39 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Declaração de Variáveis para os Elementos do Jogo
+let letra = 'X'; // X ou O
+
+function joga(casa) {
+    let celulaClicada = document.getElementById(casa).innerHTML;
+
+    if(celulaClicada == 'X'|| celulaClicada == 'O') {
+        alert('Este espaço já foi preenchido!');
+    } else {
+        document.getElementById(casa).innerHTML = letra;
+        if(letra == 'X') {
+            letra = 'O';
+        } else {
+            letra = 'X';
+        }
+    }
+}
