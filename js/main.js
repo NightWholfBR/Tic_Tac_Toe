1beac1a8f977a55677b78342d9a8302f548f82c1
NightWholfBR <127116185+NightWholfBR@users.noreply.github.com>
2023-10-02 10:51:14 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,3 +15,49 @@ function joga(casa) {
         }
     }
 }
+
+    function testa() {
+        const c1 = document.getElementById('c1').innerHTML;
+        const c2 = document.getElementById('c2').innerHTML;
+        const c3 = document.getElementById('c3').innerHTML;
+        const c4 = document.getElementById('c4').innerHTML;
+        const c5 = document.getElementById('c5').innerHTML;
+        const c6 = document.getElementById('c6').innerHTML;
+        const c7 = document.getElementById('c7').innerHTML;
+        const c8 = document.getElementById('c8').innerHTML;
+        const c9 = document.getElementById('c9').innerHTML;
+
+        if(c1 != '' && c4 != '' && c7 != ''&&
+             c1 == c4 && c4 == c7 ||
+
+             c2 != '' && c5 != '' && c8 != '' &&
+             c2 == c5 && c5 ==c8 ||
+             
+             c3 != '' && c6 != '' && c9 !== '' &&
+             c3 == c6 && c6 ==9 ||
+             
+             c1 != '' && c2 != '' && c3 != '' &&
+             c1 == c2 && c2 == c3 ||
+             
+             c4 != '' && c5 !== '' && c6 != '' &&
+             c4 == c5 && c5 == c6 ||
+             
+             c7 != '' && c8 != '' && c9 != '' &&
+             c7 == c8 && c8 == c9 ||
+             
+             c1 != '' && c5 != '' && c9 != '' &&
+             c1 == c5 && c5 == c9 ||
+             
+             c3 != '' && c5 != '' && c7 != '' &&
+             c3 == c5 && c5 == c7) {
+                alert('Você venceu!!!');
+                limpa();
+             }
+    }
+
+    function limpar () {
+        for(let i = 1; 1 <=9; i++){
+            let celula = 'c'
+            document.getElementById('c'+i).innerHTML = '';
+        }
+    }
